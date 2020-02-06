332afe42916e08296606f9f11ab5ab51098721be
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-02-06 12:49:11 -0600

    inital commit

---
+++ src/BoxList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class BoxList extends Component {
+  render() {
+    return <div>Boxlist</div>;
+  }
+}
+
+export default BoxList;
---
+++ src/NewBoxForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NewBoxForm extends Component {
+  render() {
+    return <div>NewBoxForm</div>;
+  }
+}
+
+export default NewBoxForm;
